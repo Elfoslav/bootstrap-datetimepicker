c25a78beeada1ae83c0457caecc0069247fd4404
Tomáš Hromník <tom.hromnik@gmail.com>
2013-07-10 23:02:45 +0200

    Added api.use('less', 'client') into package.js

--- package.js
+++ package.js
@@ -5,4 +5,6 @@ Package.describe({
 Package.on_use(function(api) {
 	api.add_files('lib/bootstrap-datepicker/src/js/bootstrap-datetimepicker.js', 'client');
 	api.add_files('lib/bootstrap-datepicker/src/less/bootstrap-datepicker.less', 'client');
+
+	api.use('less', 'client');
 });
